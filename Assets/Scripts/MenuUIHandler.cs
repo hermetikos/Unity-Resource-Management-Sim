275b0bac17d54ed92c649c01cbedb1947ee0ac93
Stan S <sslup@hermetikos.com>
2022-03-17 00:50:36 -0400

    Implements exit function for main menu

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -2,7 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement; 
+using UnityEditor;
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -26,5 +27,14 @@ public class MenuUIHandler : MonoBehaviour
     public void StartNewGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
